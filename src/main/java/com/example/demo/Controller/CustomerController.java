fe17f9e33bf4a3c8c8cc871ce8e7ded1554fb060
Aarti <chandravanshiaarti7@gmail.com>
2024-09-01 15:29:30 +0530

    FB-0001-api created-delete/post

--- src/main/java/com/example/demo/Controller/CustomerController.java
+++ src/main/java/com/example/demo/Controller/CustomerController.java
@@ -1,9 +1,11 @@
 package com.example.demo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,4 +37,13 @@ public class CustomerController {
     public Customer getCustomerByEmail(@RequestParam String email) {
         return customerService.getCustomerByEmail(email);
     }
+    @PutMapping("/{customerId}")
+    public Customer updateCustomer(@PathVariable Long customerId, @RequestBody Customer customerDetails) {
+        return customerService.updateCustomer(customerId, customerDetails);
+    }
+    @DeleteMapping("/{customerId}")
+    public String deleteCustomer(@PathVariable Long customerId) {
+        return customerService.deleteCustomer(customerId);
+    }
 }
+
--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +27,27 @@ public class CustomerService {
 
     public Customer getCustomerByEmail(String email) {
         return customerRepository.findByEmail(email);
+    }
+
+    public Customer updateCustomer(Long customerId, Customer customerDetails) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(customerId);
+        if (optionalCustomer.isPresent()) {
+            Customer customer = optionalCustomer.get();
+            customer.setName(customerDetails.getName());
+            customer.setEmail(customerDetails.getEmail());
+            customer.setPassword(customerDetails.getPassword());
+            customer.setBalance(customerDetails.getBalance());
+            return customerRepository.save(customer);
+        }
+        return null;
+    }
+    public String deleteCustomer(Long customerId) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(customerId);
+        if (optionalCustomer.isPresent()) {
+            customerRepository.deleteById(customerId);
+            return "Customer deleted successfully.";
+        } else {
+            return "Customer not found.";
+        }
     }
 }
